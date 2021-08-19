8592b2c7168cc8aec060e3e767d0abc9dc5e1e2a
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2021-08-19 19:28:28 +0200

    create post url

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const { property } = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,16 +11,71 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+let posts = [];
+
+
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", function(req, res){
+  res.render("home",{
+    homeContent: homeStartingContent,
+    posts: posts
+  });
 
+});
 
+app.get("/about", function(req, res){
+  res.render("about",{
+    aboutContent: aboutContent,
+  });
+});
 
+app.get("/contact", function(req, res){
+  res.render("contact",{
+    contactContent: contactContent,
+  });
+});
 
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
 
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postContent,
+  };
+  posts.push(post);
+  res.redirect("/");
+});
+
+app.get("/posts/:postTitle", function(req, res){
+  const requestedTitle = req.params.postTitle;
+  console.log("requestedTitle: " + requestedTitle)
+  posts.forEach(function(post){
+    //Get post.title and convert to lower kebab case
+    const postTile = (post.title).replace(/[^a-zA-Z0-9 ]/g, "").replace(/\s+/g,'-').toLowerCase();
+    console.log(postTile);
+    if (postTile === requestedTitle){
+      console.log("MATCH");
+    } else {
+      console.log("No match");
+    }  
+  });
+ 
+  // let post = posts.find(function(post, index){
+  //   if (post.title === requestedTitle){
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // });
+  // console.log(post.title);
+
+});
 
 
 
